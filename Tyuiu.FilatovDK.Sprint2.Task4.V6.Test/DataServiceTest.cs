f50a9ec086f608dc37ef491dfe3c1655fa448bf0
kiki769 <fiirve@bk.ru>
2024-10-15 23:08:01 +0500

    Task4

---
+++ Tyuiu.FilatovDK.Sprint2.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.FilatovDK.Sprint2.Task4.V6.Lib;
+namespace Tyuiu.FilatovDK.Sprint2.Task4.V6.Test
+{
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(1, 1);
+            Assert.AreEqual(0.667, res, 0.001);
+        }
+    }
+}
