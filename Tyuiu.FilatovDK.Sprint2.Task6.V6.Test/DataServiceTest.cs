f26385e754cc56c78c863699f03c2b5043c87e01
kiki769 <fiirve@bk.ru>
2024-10-16 00:24:46 +0500

    Task6

---
+++ Tyuiu.FilatovDK.Sprint2.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.FilatovDK.Sprint2.Task6.V6.Lib;
+namespace Tyuiu.FilatovDK.Sprint2.Task6.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindCardNameAndValue()
+        {
+
+            DataService ds = new DataService();
+            int m = 4;
+            int k = 10;
+            string res = ds.FindCardNameAndValue(m, k);
+            string wait = "Десятка черви";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
