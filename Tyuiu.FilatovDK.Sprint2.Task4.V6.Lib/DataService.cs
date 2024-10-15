e16fd658bb3b2181e181926317bf2b8feeee2d26
kiki769 <fiirve@bk.ru>
2024-10-15 23:35:17 +0500

    Task4

--- Tyuiu.FilatovDK.Sprint2.Task4.V6.Lib/DataService.cs
+++ Tyuiu.FilatovDK.Sprint2.Task4.V6.Lib/DataService.cs
@@ -1,11 +1,13 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint2;
+﻿using System.Security.AccessControl;
+using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.FilatovDK.Sprint2.Task4.V6.Lib
 {
     public class DataService : ISprint2Task4V6
     {
         public double Calculate(double x, double y)
         {
-            return x + 20 > y * 2 ? x * Math.Pow(((y + 1) / (x + 2)), x) : Math.Pow(y, 2) + 2 * x + 6 / x;
+            var res = x + 20 > y * 2 ? x * Math.Pow(((y + 1) / (x + 2)), x) : Math.Pow(y, 2) + 2 * x + 6 / x;
+            return Math.Round(res, 3);
         }
     }
 }
