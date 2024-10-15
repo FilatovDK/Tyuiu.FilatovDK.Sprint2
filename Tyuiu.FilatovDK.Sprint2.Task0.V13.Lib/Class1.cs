ec9670e2832f1f8d76b7c62767c5d6727cf0728a
kiki769 <fiirve@bk.ru>
2024-10-15 20:32:48 +0500

    Tyuiu.FilatovDK.Sprint2

--- Tyuiu.FilatovDK.Sprint2.Task0.V13.Lib/Class1.cs
+++ Tyuiu.FilatovDK.Sprint2.Task0.V13.Lib/Class1.cs
@@ -1,5 +1,4 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint2;
-
 namespace Tyuiu.FilatovDK.Sprint2.Task0.V13.Lib
 {
     public class DataService : ISprint2Task0V13
@@ -7,14 +6,16 @@ namespace Tyuiu.FilatovDK.Sprint2.Task0.V13.Lib
         public bool[] GetCompareOperations(int x, int y)
         {
             bool[] res = new bool[6];
+
             res[0] = x == y;
-            res[1] = x != y;
+            res[1] = x > y;
             res[2] = x < y;
-            res[3] = x > y;
-            res[4] = x <= y;
-            res[5] = x >= y;
+            res[3] = y + 1430 == x;
+            res[4] = y + 1 == x;
+            res[5] = x - 1430 == y;
 
             return res;
+
         }
     }
 }
