cead19702438761253998254ecacfdba0c58b668
kiki769 <fiirve@bk.ru>
2024-10-16 00:42:13 +0500

    Task7

---
+++ Tyuiu.FilatovDK.Sprint2.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.FilatovDK.Sprint2.Task7.V12.Lib
+{
+    public class DataService : ISprint2Task7V12
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if (((Math.Pow(x, 2) + Math.Pow(y, 2)) <= 1 && y >= 0))
+            {
+                res = false;
+            }
+            else
+            {
+                if (((Math.Pow(x, 2) + Math.Pow(y, 2)) <= 4 && y >= 0) && (Math.Abs(x) <= 2 && y > 0))
+                {
+                    res = true;
+                }
+                else
+                {
+                    res = false;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.FilatovDK.Sprint2.Task7.V12/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.FilatovDK.Sprint2.Task7.V12.Lib;
+namespace Tyuiu.FilatovDK.Sprint2.Task7.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine(res);
+            Console.ReadLine();
+
+        }
+    }
+}
